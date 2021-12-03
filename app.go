9cc70417e6012ecc7cd0fa1f5eb76fb3c60a86c7
jack143 <jack@inne.io>
2021-12-03 08:02:54 +0100

    Added basic GET and POST functions

---
+++ app.go
@@ -0,0 +1,82 @@
+package main
+
+// https://medium.com/codezillas/building-a-restful-api-with-go-part-1-9e234774b14d
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Roll struct {
+	ID          string `json:"id"`
+	ImageNumber string `json:"imageNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
+}
+
+var rolls []Roll
+
+func getRolls(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rolls)
+}
+
+func getRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range rolls {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newRoll Roll
+	json.NewDecoder(r.Body).Decode(&newRoll)
+	newRoll.ID = strconv.Itoa(len(rolls) + 1)
+	rolls = append(rolls, newRoll)
+	json.NewEncoder(w).Encode(newRoll)
+}
+
+func updateRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+}
+
+func main() {
+
+	rolls = append(rolls,
+		Roll{
+			ID:          "1",
+			ImageNumber: "8",
+			Name:        "Spicy Tuna Roll",
+			Ingredients: "Tuna, Chili sauce, Nori, Rice",
+		}, Roll{
+			ID:          "2",
+			ImageNumber: "6",
+			Name:        "California Roll",
+			Ingredients: "Crab, Avocado, Cucumber, Nori, Rice",
+		})
+
+	router := mux.NewRouter()
+
+	// endpoints
+	router.HandleFunc("/sushi", getRolls).Methods("GET")
+	router.HandleFunc("/sushi", getRoll).Methods("GET")
+	router.HandleFunc("/sushi", createRoll).Methods("POST")
+	router.HandleFunc("/sushi", updateRoll).Methods("POST")
+	router.HandleFunc("/sushi", deleteRoll).Methods("DELETE")
+
+	log.Fatal((http.ListenAndServe(":5000", router)))
+}
